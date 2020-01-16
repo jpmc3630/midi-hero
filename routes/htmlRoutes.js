80219230a5cda4a2c6295a07f4e43f75f1768010
brad <bcmonster@hotmail.com>
2020-01-16 20:35:48 +0800

    uploading drumkit files

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,13 +3,13 @@ var db = require("../models");
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
+    // db.Example.findAll({}).then(function(dbExamples) {
+      res.render("drums", {
+        msg: "Welcome!"
+        // examples: dbExamples
       });
     });
-  });
+  // });
 
   // Load example page and pass in an example by id
   app.get("/example/:id", function(req, res) {
